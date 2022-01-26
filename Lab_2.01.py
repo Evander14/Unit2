2dec2ee91886175bc2e55e23ea5dcc099a691450
Evander <ehrich22@students.bmchs.org>
2022-01-26 10:04:14 -0600

    added files

--- Lab_2.01.py
+++ Lab_2.01.py
@@ -3,13 +3,13 @@ In your notebook
 Predict what the following inputs will result in
 Once you have filled in the "prediction" column, check your answers in interactive mode and write the actual result.
     Input                                       Prediction                                      Result
-1.  float('1')
-2.  str(1 + '2')
-3.  str('2')
-4.  int('abc')
-5.  int(float('1.6'))
-6.  float(int(1.6))
-7.  str(float(1))
+1.  float('1')                                      '1'
+2.  str(1 + '2')                                    '12'
+3.  str('2')                                        '2'
+4.  int('abc')                                      abc
+5.  int(float('1.6'))                                1
+6.  float(int(1.6))                                  1
+7.  str(float(1))                                   '1'
 
 
 In script mode
